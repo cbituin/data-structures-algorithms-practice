5fa5a552579469b04876edc209a8cc95d3df97f4
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2022-03-02 20:39:55 -0600

    feat: implements Queue with a Linked List

--- udemy-dsa/udemy-dsa-javascript/stacks-and-queues/queue-implementation-linked-list.js
+++ udemy-dsa/udemy-dsa-javascript/stacks-and-queues/queue-implementation-linked-list.js
@@ -13,14 +13,33 @@ class Queue{
     };
 
     peek() {
-        return this;
+        return this.first;
     };
 
     enqueue(value) {
+        const newNode = new Node(value);
+        if (!this.length) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
         return this;
     }
 
     dequeue() {
+        if(!this.length) {
+            return null;
+        }
+
+        if (this.first === this.last) {
+            this.last = null;
+        }
+
+        this.first = this.first.next;
+        this.length--;
         return this;
     }
 
@@ -30,4 +49,4 @@ const myQueue = new Queue();
 myQueue.enqueue('google')
 myQueue.enqueue('udemy')
 myQueue.enqueue('discord')
-console.log(myQueue.peek())
+console.log(myQueue.dequeue())
