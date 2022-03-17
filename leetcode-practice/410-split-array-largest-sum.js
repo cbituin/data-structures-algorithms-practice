27a10ad00944d014ab00f05ccd9485d3efe03e4c
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2022-03-17 05:48:05 -0500

    feat: continuing ds+a practice

---
+++ leetcode-practice/410-split-array-largest-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {number}
+ */
+ var splitArray = function(nums, m) {
+
+    if (!nums.length) return 0;
+
+    // use BST?
+
+    // splitting array into m arrays
+
+    // take the sum of each array and compare 
+    // tracking largest sum => memoizing sum values 
+
+    // iterate through potential array configurations
+
+    // return minimize LARGEST sum
+};
